806e19f5c1f8fc619900d6260d7484080bd7adf3
Jose Miguel Arrieta Ramos <jmarrietar@gmail.com>
2020-08-17 15:06:30 -0500

    Add long description Add version

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup, find_packages
 
 REQUIRED_PACKAGES = ["tensorflow == 2.2.0"]
 
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="tfr_image",
-    version="1.0",
+    version="1.1",
     license='MIT',
     url='https://github.com/jmarrietar/tfr_image',
     description="TFRimage is minimal tool to create TFRecords from a small dataset of images",
